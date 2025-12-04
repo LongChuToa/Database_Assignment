5ca603daf7486fc32d07bf7a944d7231d1b1ca9a
LongChuToa <ghoulkysinh184@gmail.com>
2025-12-04 20:14:53 +0700

    env free

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,12 @@ from fastapi.middleware.cors import CORSMiddleware
 # Import các router
 from .api.v1.endpoints import crud_items, functions, lists
 
+from security.decrypt_loader import load_encrypted_env 
+
+# --- NẠP BIẾN MÔI TRƯỜNG MÃ HÓA ---
+load_encrypted_env()
+# -----------------------------------
+
 app = FastAPI(title="BK-LMS Backend API")
 
 # Cấu hình CORS
@@ -17,10 +23,10 @@ app.add_middleware(
 )
 
 # --- ĐĂNG KÝ ROUTER ---
-# 1. Router cho Lớp học (CRUD, Search) - Source 45, 46
+# 1. Router cho Lớp học (CRUD, Search)
 app.include_router(crud_items.router, prefix="/api/v1", tags=["Classes"])
 
-# 2. Router cho Hàm/Báo cáo - Source 50
+# 2. Router cho Hàm/Báo cáo
 app.include_router(functions.router, prefix="/api/v1", tags=["Reports"])
 
 # 3. Router cho Danh sách phụ (Dropdown)
